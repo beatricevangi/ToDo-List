8b2104286c89c4413efdbfd5595a5c2ce7e4ccb1
beatricevangi <beatricevangi99@gmail.com>
2022-06-29 19:20:27 +0200

    Added google testing

---
+++ test/runAllTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by beatrice on 29/06/22.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
